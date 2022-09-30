7c21da3b3eb2f888f88b1847de8dbb725ebaa054
Dave Godbold (Credera) <dave.godbold@credera.co.uk>
2022-09-30 14:17:43 +0100

    Use param args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-rover  -workingDir "/github/workspace/terraform" \
-    -tfBackendConfig "backend.config" \
-    -tfVarsFile "demo.tfvars" \
+# rover  -workingDir "/github/workspace/terraform" \
+#     -tfBackendConfig "backend.config" \
+#     -tfVarsFile "demo.tfvars" \
+#     -standalone true
+
+rover -workingDir "$1" \
+    -tfBackendConfig "$2" \
+    -tfVarsFile "$3" \
     -standalone true
-
-
-# rover -standalone true \
-#     -workingDir "$1" \
-#     -tfBackendConfig "$2" \
-#     -tfVarsFile "$3" \
 
 unzip -o rover.zip -d ./rover-website
