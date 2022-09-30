5f32a2713ddaba2cab1046ca2d01c865a51cda34
Dave Godbold (Credera) <dave.godbold@credera.co.uk>
2022-09-30 16:14:37 +0100

    Try setup gh pages deployment with git

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM im2nguyen/rover
 
 RUN apk add unzip
+RUN apk add git
 
 ADD entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,16 @@ rover -workingDir "$1" \
     -tfVarsFile "$3" \
     -standalone true
 
-unzip -o rover.zip -d ./rover-website
+ROVER_FOLDER="./rover"
+
+unzip -o rover.zip -d $ROVER_FOLDER
+
+#BRANCH="$4"
+BRANCH="testing-upload"
+OIDC_TOKEN="$5"
+
+git checkout --orphan $BRANCH
+git reset --hard
+git commit --no-verify --allow-empty -m Initial gh-pages commit
+git add --all $ROVER_FOLDER
+git push --force $BRANCH:$BRANCH
