8d529c0fe130e1d2dc8fed9b50225e1fef9949f5
Dave Godbold (Credera) <dave.godbold@credera.co.uk>
2022-09-30 09:59:27 +0100

    Use args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
 
-rover -tfBackendConfig "backend.config" -tfVarsFile "demo.tfvars" -standalone true
+# rover -tfBackendConfig "backend.config" -tfVarsFile "demo.tfvars" -standalone true
 
-#rover -standalone true \
-#    -workingDir "$1" \
-#    -tfBackendConfig "$2" \
-#    -tfVarsFile "$3" \
+rover -standalone true \
+    -workingDir "$1" \
+    -tfBackendConfig "$2" \
+    -tfVarsFile "$3" \
 
 unzip -o rover.zip -d ./rover-website
