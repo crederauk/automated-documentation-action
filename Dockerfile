12c56b828515baf083b26d1a8515690bc73cd91e
Dave Godbold (Credera) <dave.godbold@credera.co.uk>
2022-11-04 12:42:47 +0000

    Revert

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM im2nguyen/rover
 RUN apk add unzip
 RUN apk add git
 
-COPY web /
 ADD entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["sh","/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,12 @@ ROVER_FOLDER="./terraform-rover"
 unzip -o rover.zip -d $ROVER_FOLDER
 
 BRANCH="$4"
-#OIDC_TOKEN="$5"
 
 git config --global user.name "Automated Documentation"
 git config --global user.email "automated.documentation@users.noreply.github.com"
 
 git checkout --orphan $BRANCH
 git reset --hard
-git add --all
-# git add --all $ROVER_FOLDER/\*
+git add --all $ROVER_FOLDER/\*
 git commit --no-verify --allow-empty -m 'Initial gh-pages commit'
 git push -f origin $BRANCH
