e25868e7e4e9b587d0247731cdc52c2cfb039015
Dave Godbold (Credera) <dave.godbold@credera.co.uk>
2022-11-04 12:03:37 +0000

    Try use repo base

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,18 @@ rover -workingDir "$1" \
     -tfVarsFile "$3" \
     -standalone true
 
-# unzip to the current directory as gh pages expects index.html etc to be there
-unzip -o rover.zip
+ROVER_FOLDER="./rover"
+
+unzip -o rover.zip -d $ROVER_FOLDER
 
 BRANCH="$4"
+#OIDC_TOKEN="$5"
 
 git config --global user.name "Automated Documentation"
 git config --global user.email "automated.documentation@users.noreply.github.com"
 
 git checkout --orphan $BRANCH
 git reset --hard
-git add --all 
+git add --all :/$ROVER_FOLDER/\*
 git commit --no-verify --allow-empty -m 'Initial gh-pages commit'
 git push -f origin $BRANCH
