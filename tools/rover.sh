8a8be08b247e85eb480eae9a290743ed53aef2b5
Dave Godbold (Credera) <dave.godbold@credera.co.uk>
2022-11-25 15:38:00 +0000

    Rover in subfolder

--- tools/rover.sh
+++ tools/rover.sh
@@ -1,14 +1,20 @@
 #!/bin/ash
 
+# rover subfolder
+mkdir rover && cd $_
+
 # install rover
 echo "Cloning Rover Git repo"
-git clone https://github.com/im2nguyen/rover.git "rover/source"
+git clone https://github.com/im2nguyen/rover.git "./source"
 
 echo "Install Rover dependencies and build"
-cd "rover/source/ui"
+cd "source/ui"
 npm install
 npm run build
 
 echo "Installing Rover in container"
 cd ..
 go install
+
+# go back up a level
+cd ..
